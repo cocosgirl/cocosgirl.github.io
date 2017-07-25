743b4fa58b33db984b1cb73a91e8690c74b7397d
cocosgirl <zyy1152134749@gmail.com>
2017-07-25 21:50:59 +0800

    合并分支2017年 7月25日 星期二 21时50分59秒 CST

--- myMerge.sh
+++ myMerge.sh
@@ -3,18 +3,7 @@
 dt=`date`
 echo $dt
 echo 'start merge--------------'
-git checkout origin/hexo 2017
-git checkout origin/hexo about
-git checkout origin/hexo archives
-git checkout origin/hexo categories
-git checkout origin/hexo css
-git checkout origin/hexo images
-git checkout origin/hexo js
-git checkout origin/hexo lib
-git checkout origin/hexo page
-git checkout origin/hexo pageName
-git checkout origin/hexo tags
-git checkout origin/hexo index.html
+cp -r public/ ./
 git add *
 git commit -m "合并分支$dt"
 git push -u origin master
