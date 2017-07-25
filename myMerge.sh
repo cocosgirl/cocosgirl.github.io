778c33512bb9a5f4d87e1563003d3ba5bf36120f
cocosgirl <zyy1152134749@gmail.com>
2017-07-25 10:12:00 +0800

    新增加合并脚本

---
+++ myMerge.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+dt=`date`
+echo $dt
+echo 'start merge--------------'
+git checkout origin/hexo 2017
+git checkout origin/hexo about
+git checkout origin/hexo archives
+git checkout origin/hexo categories
+git checkout origin/hexo css
+git checkout origin/hexo images
+git checkout origin/hexo js
+git checkout origin/hexo lib
+git checkout origin/hexo page
+git checkout origin/hexo pageName
+git checkout origin/hexo tags
+git checkout origin/hexo index.html
+git commit -m "合并分支$dt"
+git push -u origin master
+echo 'end merge--------------'
